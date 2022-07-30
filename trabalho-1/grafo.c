e94b147cecb07226c2f073aef460c3a5c89cdf3c
marisasel <marisafranco@gmail.com>
2022-07-30 19:15:39 -0300

    Funções triviais OK, implementadas a partir de funções da lib cgraph

--- trabalho-1/grafo.c
+++ trabalho-1/grafo.c
@@ -1,56 +1,160 @@
+//*******************************************
+//   Grafos:  Trabalho 1 - 2022.1           *
+//   Curso:   Informática Biomédica - UFPR  *
+//   Alunos:  Giovani G. Marciniak          *
+//            Marisa Sel Franco             *
+//*******************************************
+
 #include <stdio.h>
 #include "grafo.h"
+#include <stdlib.h>
+
+typedef Agedge_t *aresta;
 
 //------------------------------------------------------------------------------
 grafo le_grafo(void) {
 
-  return agread(stdin, NULL); 
+  grafo g = agread(stdin, NULL);
+
+  if (!g) {
+    fprintf(stderr, "Erro: não foi possível ler o grafo.\n");
+    exit(-1);
+  }
+
+  return g; 
 }
+
 //------------------------------------------------------------------------------
 void destroi_grafo(grafo g) {
   
+  if (!g) {
+    fprintf(stderr, "Erro: não há grafo em memória.\n");
+    exit(-2);
+  }
+  agfree(g, NULL);
+
   return 1;
 }
 //------------------------------------------------------------------------------
 grafo escreve_grafo(grafo g) {
   
-  return agwrite(g, stdout);
+  if (!g) {
+    fprintf(stderr, "Erro: não há grafo em memória.\n");
+    exit(-2);
+  }
+  agwrite(g, stdout);
+
+  return g;
 }
 
 // -----------------------------------------------------------------------------
 int n_vertices(grafo g) {
   
-  return 0;
-}
+  int num_vertices;
 
+  if (!g) {
+    fprintf(stderr, "Erro: não há grafo em memória.\n");
+    exit(-2);    
+  }
+  num_vertices = agnnodes(g);
+
+  return num_vertices;
+}
 // -----------------------------------------------------------------------------
 int n_arestas(grafo g) {
-  
-  return 0;
+
+  int num_arestas;
+
+  if (!g) {
+    fprintf(stderr, "Erro: não há grafo em memória.\n");
+    exit(-2);    
+  }
+  num_arestas = agnedges(g);
+
+  return num_arestas;
 }
 
 // -----------------------------------------------------------------------------
 int grau(vertice v, grafo g) {
-  
-  return 0;
+
+  int grau_vertice;
+
+  if (!g) {
+    fprintf(stderr, "Erro: não há grafo em memória.\n");
+    exit(-2);    
+  }  
+  if (!v) {
+    fprintf(stderr, "Erro: vértice inválido.\n");
+    exit(-3);    
+  }
+  grau_vertice = agdegree(g, v, TRUE, TRUE);    // conta tanto arestas de entrada quanto de saída
+
+  return grau_vertice;
 }
 
 // -----------------------------------------------------------------------------
 int grau_maximo(grafo g)  {
-  
-  return 0;
+
+  vertice v;
+  int grau_vertice, maior_grau;
+
+  if (!g) {
+    fprintf(stderr, "Erro: não há grafo em memória.\n");
+    exit(-2);    
+  }
+
+  maior_grau = grau(agfstnode(g),g);           // chuta que o 1º vértice tem o maior grau
+
+  // laço que percorre os vértices para calcular o grau máximo
+  for (v = agfstnode(g); v; v = agnxtnode(g, v)) {
+    grau_vertice = grau(v, g);
+    if (grau_vertice > maior_grau)
+      maior_grau = grau_vertice;    
+  }
+
+  return maior_grau;
 }
 
 // -----------------------------------------------------------------------------
 int grau_minimo(grafo g)  {
-  
-  return 0;
+
+  vertice v;
+  int grau_vertice, menor_grau;
+
+  if (!g) {
+    fprintf(stderr, "Erro: não há grafo em memória.\n");
+    exit(-2);    
+  }
+
+  menor_grau = grau(agfstnode(g), g);           // chuta que o 1º vértice tem o menor grau
+
+  // laço que percorre os vértices para calcular o grau mínimo
+  for (v = agfstnode(g); v; v = agnxtnode(g, v)) {
+    grau_vertice = grau(v, g);
+    if (grau_vertice < menor_grau)
+      menor_grau = grau_vertice;    
+  }
+
+  return menor_grau;
 }
 
 // -----------------------------------------------------------------------------
 int grau_medio(grafo g) {
-  
-  return 0;
+
+  vertice v;
+  int somatorio_graus = 0, grau_medio;
+
+  if (!g) {
+    fprintf(stderr, "Erro: não há grafo em memória.\n");
+    exit(-2);    
+  }
+
+  // laço que percorre os vértices para fazer o somatório dos graus
+  for (v = agfstnode(g); v; v = agnxtnode(g, v))
+    somatorio_graus += grau(v, g);
+  grau_medio = somatorio_graus / n_vertices(g); // calcula o grau médio de g
+
+  return grau_medio;
 }
 
 // -----------------------------------------------------------------------------
@@ -94,4 +198,3 @@ grafo complemento(grafo g) {
   
   return NULL;
 }
-
--- trabalho-1/teste.c
+++ trabalho-1/teste.c
@@ -5,10 +5,27 @@
 
 int main(void) {
 
+  int num_v, num_a, g_max, g_min, g_medio;
+
   grafo g = le_grafo();
+  printf("O grafo foi lido corretamente e tem:\n");
 
+  num_v = n_vertices(g);
+  printf("\t %d vértices;\n", num_v);
+
+  num_a = n_arestas(g);
+  printf("\t %d arestas;\n", num_a);
+
+  g_max = grau_maximo(g);
+  printf("\t grau máximo igual a %d;\n", g_max);
+
+  g_min = grau_minimo(g);
+  printf("\t grau mínimo igual a %d;\n", g_min);
+
+  g_medio = grau_medio(g);
+  printf("\t grau médio igual a %d.\n\n", g_medio);
+    
   escreve_grafo(g);
-
   destroi_grafo(g);
 
   return 0;
